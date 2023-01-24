d20a45737e6d1cca2a693685e7b285ea270aa268
rithudivakar <111083162+rithudivakar@users.noreply.github.com>
2023-01-24 18:22:57 +0530

    Add files via upload

---
+++ api/routes/mysql_connection.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql2');
+
+const con = mysql.createConnection({
+  host: "172.18.0.1",
+  port: 13306,
+  user: "root",
+  password: "password",
+  database: "shop"
+});
+exports.con = con
