cb9fb32644b2303cf046a2f29db1525c56075f67
rithudivakar <111083162+rithudivakar@users.noreply.github.com>
2023-01-31 18:16:46 +0530

    Add files via upload

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const mysql_connection = require('./mysql_connection');
-// import express from 'express';
+const promClient = require('prom-client');
 const router = express.Router();
+
+const getcounter = new promClient.Counter({
+  name: 'get_counter',
+  help: 'This is my get counter'
+});
+
+const getallcounter = new promClient.Counter({
+    name: 'get_all_counter',
+    help: 'This is my get all counter'
+});
+
+const postcounter = new promClient.Counter({
+    name: 'post_counter',
+    help: 'This is my post counter'
+});
+const counter = new promClient.Counter({
+    name: 'my_route_counter',
+    help: 'This is my route counter'
+});
 
 //GET ALL
 router.get('/', (req, res, next) => {
+    getallcounter.inc();
     var sql = "SELECT * FROM products";
     mysql_connection.con.query(sql, function(error, result){
         if(error) throw error;
@@ -18,6 +38,7 @@ router.get('/', (req, res, next) => {
 
 //INSERT INTO TABLE
 router.post('/', (req, res, next) => {
+    postcounter.inc();
     const product = {
         prod_id: req.body.prod_id,
         prod_name: req.body.prod_name,
@@ -41,12 +62,13 @@ router.post('/', (req, res, next) => {
 
 //GET BY ID
 router.get('/:prod_id', (req, res, next) => {
-    const prod_Id = req.params.prod_Id;
+    getcounter.inc();
+    const prod_id = req.params.prod_id;
     var sql = "SELECT * FROM products WHERE prod_id = ?";
-    mysql_connection.con.query(sql, prod_Id, function(error, result){
+    mysql_connection.con.query(sql, prod_id, function(error, result){
         if(error) throw error;
         res.status(200).json({
-            message : 'Product in the table with id ',
+            message : 'Product in the table with id '+prod_id+'',
             product: result
         });
     });
@@ -62,13 +84,30 @@ router.get('/:prod_id', (req, res, next) => {
     // }
 });
 
-router.patch('/:productId', (req, res,next) => {
+router.patch('/:prod_id', (req, res,next) => {
+    counter.inc();
     res.status(200).json({
         message : 'Updated product!'
     });
+    // const prod_id = req.params.prod_id;
+    // const product = {
+    //     prod_id: req.body.prod_id,
+    //     prod_name: req.body.prod_name,
+    //     price: req.body.price
+            
+    // };
+    // var sql = "UPDATE products SET prod_id = ?, prod_name = ?, price = ? WHERE prod_id = ?"
+    // mysql_connection.con.query(sql, product, prod_id, function(error, result){
+    //     if(error) throw error;
+    //     res.status(200).json({
+    //         message : 'Updated Product with id '+prod_id+'',
+    //         product: result
+    //     });
+    // });
 });
 
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:prod_id', (req, res, next) => {
+    counter.inc();
     res.status(200).json({
         message : 'Deleted product!'
     });
