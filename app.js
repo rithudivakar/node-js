d6e0693432b3dbfcea1c4d296d6008d22ae28340
rithudivakar <111083162+rithudivakar@users.noreply.github.com>
2023-02-02 13:53:02 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const promClient = require("prom-client");
+const client = require("prom-client");
+const register = new client.Registry();
+
+
 // const actuator = require('express-actuator');
 // app.use(actuator());
 const port = process.env.PORT || "8080";
@@ -60,13 +63,66 @@ app.use('/orders', orderRoutes);
 //     res.set('Content-Type', promClient.register.contentType);
 //     res.end(promClient.register.metrics());
 // });
+client.collectDefaultMetrics({
+    app: 'node-application-monitoring-app',
+    prefix: 'node_',
+    timeout: 10000,
+    gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5],
+    register
+});
+app.get('/metrics', async (req, res) => {
+    res.setHeader('Content-Type', register.contentType);
+    res.send(await register.metrics());
 
-app.get('/metrics', (req, res) => {
-    promClient.register.metrics().then((metrics) => {
-      res.set('Content-Type', promClient.register.contentType);
-      res.end(metrics);
-    });
+});
+const httpRequestTimer = new client.Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['method', 'route', 'code'],
+    buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10] // 0.1 to 10 seconds
   });
+  register.registerMetric(httpRequestTimer);
+
+  const createDelayHandler = async (req, res) => {
+    if ((Math.floor(Math.random() * 100)) === 0) {
+      throw new Error('Internal Error')
+    }
+    // Generate number between 3-6, then delay by a factor of 1000 (miliseconds)
+    const delaySeconds = Math.floor(Math.random() * (6 - 3)) + 3
+    await new Promise(res => setTimeout(res, delaySeconds * 1000))
+    res.end('Slow url accessed!');
+  };
+  app.get('/metrics', async (req, res) => {
+    // Start the HTTP request timer, saving a reference to the returned method
+    const end = httpRequestTimer.startTimer();
+    // Save reference to the path so we can record it when ending the timer
+    const route = req.route.path;
+      
+    res.setHeader('Content-Type', register.contentType);
+    res.send(await register.metrics());
+  
+    // End timer and add labels
+    end({ route, code: res.statusCode, method: req.method });
+  });
+  
+  // 
+  app.get('/slow', async (req, res) => {
+    const end = httpRequestTimer.startTimer();
+    const route = req.route.path;
+    await createDelayHandler(req, res);
+    end({ route, code: res.statusCode, method: req.method });
+  });
+  
+  
+
+// app.get('/metrics', (req, res) => {
+//     promClient.register.metrics().then((metrics) => {
+//       res.set('Content-Type', promClient.register.contentType);
+//       res.end(metrics);
+//     });
+//   });
+
+
 
 
 //Increment the metric:
